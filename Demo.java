c82360b8c2ac216d415a9403c72df7fc882f686c
mozammilrahman52 <mozammilrahman52@gmail.com>
2024-01-28 13:27:26 +0530

    hellow World commited

--- Demo.java
+++ Demo.java
@@ -1,7 +1,9 @@
 public class Demo {
     public static void main(String[] args) {
         System.out.println("Hello");
-        System.out.println("World");
+        System.out.println("Hello");
+        System.out.println("Hello");
+
     }
 
 }
